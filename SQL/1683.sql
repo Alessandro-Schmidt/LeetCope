349b753d66eed495170bc88e2249d01c096a796e
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2025-04-21 20:59:17 -0300

    1683. Invalid Tweets

---
+++ SQL/1683.sql
@@ -0,0 +1,15 @@
+"""
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of alphanumeric characters, '!', or ' ' and no other special characters.
+This table contains all the tweets in a social media app.
+"""
+
+-- Answer:
+select tweet_id from Tweets
+where LENGTH(content) > 15;
